2f3127889dac5d0857ac0811d6eae7d8081ce19b
mrke <mrke@unimelb.edu.au>
2025-09-04 10:47:10 +1000

    setting up module

--- src/FluidProperties.jl
+++ src/FluidProperties.jl
@@ -6,8 +6,6 @@ end
 
 using Unitful, UnitfulMoles
 
-using Unitful: °, rad, °C#, K, Pa, kPa, MPa, J, kJ, W, L, g, kg, cm, m, s, hr, d, mol, mmol, μmol, σ, R
-
 export vapour_pressure, wet_air, dry_air, get_pressure, get_λ_evap, water_prop
 
 include("fluid_properties.jl")
---
+++ tests.jl
@@ -0,0 +1,5 @@
+using FluidProperties
+using Unitful
+
+elevation = 100.0u"m"
+get_pressure(elevation)
