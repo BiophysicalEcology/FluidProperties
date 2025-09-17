102a02fbbeed8a292dd046780f38cf50130bb0e5
Rafael Schouten <rafaelschouten@gmail.com>
2025-09-17 12:46:00 +1000

    reorganise a little

--- src/FluidProperties.jl
+++ src/FluidProperties.jl
@@ -1,15 +1,21 @@
 module FluidProperties
 
-function __init__()\
-    Unitful.register(FluidProperties)
-end
-
 using Unitful, UnitfulMoles
 
 using Unitful: ustrip, uconvert
 
 export vapour_pressure, wet_air, dry_air, get_pressure, get_λ_evap
 
+@compound H2O
+@compound O2
+@compound CO2
+@compound N2
+
+include("vapor_pressure.jl")
 include("fluid_properties.jl")
+
+function __init__()\
+    Unitful.register(FluidProperties)
+end
 
 end
